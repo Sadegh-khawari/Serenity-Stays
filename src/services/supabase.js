345e2f8ad9f91eb83fb73b14770c79220cc4759f
Sadegh Khavari <sadeghkhawari@outlook.com>
2024-10-09 16:08:35 +0330

    initilize the git repo

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://qwvzcbjcjfwiysnpyple.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3dnpjYmpjamZ3aXlzbnB5cGxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzMTQ1MzcsImV4cCI6MjA0Mzg5MDUzN30.QDvgVdjhrnjqcmx-7BzZmMoA64obXKIttT0cHYFEwjo";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
