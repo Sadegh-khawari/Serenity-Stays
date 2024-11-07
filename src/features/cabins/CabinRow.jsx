bddf41b2a2987b56068495b90a1c93796136a915
Sadegh Khavari <sadeghkhawari@outlook.com>
2024-11-08 00:18:16 +0330

    Build a reusable tabel, header part

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,26 +1,27 @@
 import styled from "styled-components";
 
 import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal.jsx";
+import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
+import Table from "../../ui/Table.jsx";
 
 import {formatCurrency} from "../../utils/helpers";
 import {useDeleteCabin} from "./useDeleteCabin";
 import {HiPencil, HiTrash} from "react-icons/hi";
 import {HiSquare2Stack} from "react-icons/hi2";
 import {useCreateCabin} from "./useCreateCabin.js";
-import Modal from "../../ui/Modal.jsx";
-import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
 
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
-
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
+// const TableRow = styled.div`
+//     display: grid;
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//     column-gap: 2.4rem;
+//     align-items: center;
+//     padding: 1.4rem 2.4rem;
+//
+//     &:not(:last-child) {
+//         border-bottom: 1px solid var(--color-grey-100);
+//     }
+// `;
 
 const Img = styled.img`
     display: block;
@@ -76,7 +77,7 @@ function CabinRow({cabin}) {
     }
 
     return (
-            <TableRow role="row">
+            <Table.Row columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
                 <Img src={image}/>
                 <Cabin>{name}</Cabin>
                 <div>Fits up to {maxCapacity} guests</div>
@@ -115,7 +116,7 @@ function CabinRow({cabin}) {
                         </Modal.Window>
                     </Modal>
                 </div>
-            </TableRow>
+            </Table.Row>
     );
 }
 
