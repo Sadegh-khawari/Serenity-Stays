350a9e35e9e48b964ab5e1a7919e7d95d388c36f
Sadegh Khavari <sadeghkhawari@outlook.com>
2024-11-08 16:23:10 +0330

    Build the context menu functionality

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,6 +5,7 @@ import CabinRow from "./CabinRow";
 import Table from "../../ui/Table.jsx";
 
 import {useCabins} from "./useCabins.js";
+import Menus from "../../ui/Menus.jsx";
 
 
 const TableHeader = styled.header`
@@ -22,24 +23,26 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-    const {isLoading,  cabins} = useCabins();
+    const {isLoading, cabins} = useCabins();
 
     if (isLoading) return <Spinner/>;
 
     return (
-        <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
-            <Table.Header>
-                <div></div>
-                <div>Cabin</div>
-                <div>Capacity</div>
-                <div>Price</div>
-                <div>Discount</div>
-                <div></div>
-            </Table.Header>
-            <Table.Body data={cabins} render={(cabin) => (
-                <CabinRow key={cabin.id} cabin={cabin}/>
-            )}/>
-        </Table>
+        <Menus>
+            <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+                <Table.Header>
+                    <div></div>
+                    <div>Cabin</div>
+                    <div>Capacity</div>
+                    <div>Price</div>
+                    <div>Discount</div>
+                    <div></div>
+                </Table.Header>
+                <Table.Body data={cabins} render={(cabin) => (
+                    <CabinRow key={cabin.id} cabin={cabin}/>
+                )}/>
+            </Table>
+        </Menus>
     );
 }
 
