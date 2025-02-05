0f0e9222260a427e543a730d3a88d957f579e160
Sadegh Khavari <sadeghkhawari@outlook.com>
2025-02-05 17:41:08 +0330

    Update user
    Add the update user data and password feature.

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -48,4 +48,34 @@ async function logout() {
   if (error) throw new Error(error.message);
 }
 
-export { login, getCurrentUser, logout, signup };
+async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update password OR fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2. Upload the avatar Image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. Updaate avatar in user
+  const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+  return updatedUser;
+}
+
+export { login, getCurrentUser, logout, signup, updateCurrentUser };
